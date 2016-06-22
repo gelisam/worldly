60c234e783cc64daf2ff2e5e37043dc8c35c3526
Samuel Gélineau <gelisam+github@gmail.com>
2016-06-21 22:23:32 -0400

    Monad instance, where possible

--- src/Language/Wordly.hs
+++ src/Language/Wordly.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Language.Wordly where
 
 import Bound
+import Control.Monad
 
 
 data Type
@@ -13,8 +15,25 @@ data STerm a
   | Ann (CTerm STerm a) Type       -- ^ type annotation
   | UnitV                          -- ^ the only value of type UnitT
   | App (STerm a) (CTerm STerm a)  -- ^ function application
+  deriving (Functor)
 
 -- terms which are Checked against a type
 data CTerm sTerm a
   = STerm (sTerm a)
   | Lam (Scope () sTerm a) -- ^ lambda abstraction
+  deriving (Functor)
+
+
+instance Applicative STerm where
+  pure = Var
+  (<*>) = ap
+
+instance Monad STerm where
+  Var v   >>= f = f v
+  Ann x t >>= f = Ann (x >>>= f) t
+  UnitV   >>= _ = UnitV
+  App x y >>= f = App (x >>= f) (y >>>= f)
+
+instance Bound CTerm where
+  STerm x >>>= f = STerm (x >>= f)
+  Lam x   >>>= f = Lam (x >>>= f)
