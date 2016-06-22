e3d7eb732944d9c063120cbb5cd9663e1e445752
Samuel Gélineau <gelisam+github@gmail.com>
2016-06-21 22:12:35 -0400

    represent variables using bound

--- src/Language/Wordly.hs
+++ src/Language/Wordly.hs
@@ -1,18 +1,20 @@
 module Language.Wordly where
 
+import Bound
+
 
 data Type
   = UnitT          -- ^ unit type
   | Arr Type Type  -- ^ function type
 
--- terms which are Checked against a type
-data CTerm a
-  = STerm (STerm a)
-  | Lam (STerm (Maybe a)) -- ^ lambda abstraction
-
 -- terms which can Synthesize their type
 data STerm a
   = Var a
-  | Ann (CTerm a) Type       -- ^ type annotation
-  | UnitV                    -- ^ the only value of type UnitT
-  | App (STerm a) (CTerm a)  -- ^ function application
+  | Ann (CTerm STerm a) Type       -- ^ type annotation
+  | UnitV                          -- ^ the only value of type UnitT
+  | App (STerm a) (CTerm STerm a)  -- ^ function application
+
+-- terms which are Checked against a type
+data CTerm sTerm a
+  = STerm (sTerm a)
+  | Lam (Scope () sTerm a) -- ^ lambda abstraction
